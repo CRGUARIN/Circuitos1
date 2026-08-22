99bc9d6024d3699b97a231378f8470dee15da323
Maria Paula <paula.mejia@udea.edu.com>
2026-08-22 01:10:49 -0500

    Punto 2)c

---
+++ Laboratorio/Lab 1/transfa_02_c.m
@@ -0,0 +1,61 @@
+% Vector tiempo
+t = linspace(0,2,1001);
+
+% Señal original
+f = sin(2*pi*t);
+
+% Solicitar valor de A
+A = input("Ingrese el valor de A entre -2 y 2: ");
+
+while A < -2 || A > 2
+    disp("El valor de A debe estar entre -2 y 2")
+    A = input("Ingrese nuevamente el valor de A: ");
+end
+
+%señal y(t)
+y = A*f;
+
+% Determinar el título según el valor de A
+if A > 1
+    texto = "Amplificación sin reflexión";
+
+elseif A > 0 && A < 1
+    texto = "Atenuación sin reflexión";
+
+elseif A < -1
+    texto = "Amplificación con reflexión";
+
+elseif A > -1 && A < 0
+    texto = "Atenuación con reflexión";
+
+elseif A == -1
+    texto = "Solo reflexión de amplitud";
+
+elseif A == 1
+    texto = "Original";
+
+elseif A == 0
+    texto = "Señal nula";
+end
+
+% f(t)
+subplot(2,1,1)
+plot(t, f, 'r', 'LineWidth', 2)
+xlabel("Tiempo t")
+ylabel("f(t)")
+title("Señal original f(t) = sin(2*pi*t)")
+grid on
+ylim([-1.5 1.5])
+xlim([0 2])
+yline(0,'k--')
+
+% y(t)
+subplot(2,1,2)
+plot(t, y, 'm', 'LineWidth', 2)
+xlabel("Tiempo t")
+ylabel("y(t)")
+title(texto)
+grid on
+ylim([-1.5 1.5])
+xlim([0 2])
+yline(0,'k--')
